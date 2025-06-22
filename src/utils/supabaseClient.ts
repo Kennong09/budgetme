4be8b3451a72f5ae54a889a33dd68947aa1c37e5
Kennong09 <kennethbuely@gmail.com>
2025-06-22 16:48:09 +0800

    feat: update url

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // Try to get values from environment variables first
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://rguoppydsikvbfvjxosq.supabase.co';
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJndW9wcHlkc2lrdmJmdmp4b3NxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA1Nzc4MjYsImV4cCI6MjA2NjE1MzgyNn0.7cZSGwVBqFGaB82YgWzH2cALXdMLtBmOJ4cJiaSJCEc';
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJndW9wcHlkc2lrdmJmdmp4b3NxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MDU3NzgyNiwiZXhwIjoyMDY2MTUzODI2fQ.z1WjOyJgoGu-YlRzMZku9IhsxGgnQFFr9cMyPLyyrUI';
 
 // Logging for debugging
 console.log('Supabase URL:', supabaseUrl);
